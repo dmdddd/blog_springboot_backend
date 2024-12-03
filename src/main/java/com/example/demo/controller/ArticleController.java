65283d72b3ab2629eb4de1f1114c9dd84a3762d2
Daniel <dk1989@gmail.com>
2024-12-03 16:52:40 -0600

    Removing obsolete imports

--- src/main/java/com/example/demo/controller/ArticleController.java
+++ src/main/java/com/example/demo/controller/ArticleController.java
@@ -1,22 +1,16 @@
 package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Optional;
 
 import com.example.demo.dto.ArticleResponseDto;
-import com.example.demo.exceptions.ArticleNotFoundException;
-import com.example.demo.model.Article;
 import com.example.demo.service.ArticleService;
 import org.springframework.web.bind.annotation.PutMapping;
 
@@ -30,7 +24,7 @@ public class ArticleController {
     // GET endpoint to retrieve all articles
     @GetMapping
     public ResponseEntity<List<ArticleResponseDto>> getAllArticles() {
-        
+
         List<ArticleResponseDto> articles = articleService.getAllArticles();
         return ResponseEntity.ok(articles); // 200 OK with the articles
     }
