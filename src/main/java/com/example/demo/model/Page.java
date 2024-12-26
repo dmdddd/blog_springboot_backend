48aa8f338538d24ba2fab69d8e8e8823861fe972
dmdddd <dk198@gmail.com>
2024-12-26 16:27:51 -0600

    Removing some old imports from the Page

--- src/main/java/com/example/demo/model/Page.java
+++ src/main/java/com/example/demo/model/Page.java
@@ -1,8 +1,6 @@
 package com.example.demo.model;
 
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
